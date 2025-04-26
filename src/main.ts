04a8e1b35ba984060bbad411a22c776d8d0f321d
Amir Maghami <Maghami.A1987@gmail.com>
2025-04-26 17:04:36 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { initI18n } from '@/i18n'
 
 const app = createApp(App)
+const i18n = initI18n
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n) // خیلی مهمه که این باشه
 
 app.mount('#app')
