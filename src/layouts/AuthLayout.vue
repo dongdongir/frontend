54b7eae2ffeed043177a36729f7b0dfedf2a6ea6
Amir Maghami <Maghami.A1987@gmail.com>
2025-04-26 15:45:19 +0330

    fix: Switch on base vue

--- src/layouts/AuthLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="auth-layout">
-    <IonRouterOutlet />
-  </div>
-</template>
-<script setup lang="ts">
-import { IonRouterOutlet } from "@ionic/vue";
-</script>
-<style lang="scss"></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-/// <reference types="vitest" />
+import { fileURLToPath, URL } from 'node:url'
 
-import legacy from "@vitejs/plugin-legacy";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), legacy(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-});
+})
